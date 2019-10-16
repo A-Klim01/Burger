f690de5db52b802db7ad98ce636a0183152f1960
Alojzy Klim <adklim@gmail.com>
2019-10-16 18:48:26 -0500

    inital files + code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('hamburger');
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('turkey burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', true);
+INSERT INTO burgers (burger_name) VALUES ('impossible burger');
