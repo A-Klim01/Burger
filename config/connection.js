0612da5bf3053e6a8013557eb587d00acab58800
Alojzy Klim <adklim@gmail.com>
2019-10-21 15:27:36 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if(process.env.JAWS_DB){
+  var connection = process.env.JAWS_DB;
+}else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
