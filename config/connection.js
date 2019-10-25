a2e001cc5b160632f8c841ad7239d576e5604cf9
Alojzy Klim <adklim@gmail.com>
2019-10-25 15:01:24 -0500

    jaws_url properly linked

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,17 @@
 var mysql = require("mysql");
 var connection;
 
-// if(process.env.JAWS_DB){
-  connection = mysql.createConnection(process.env.JAWS_DB);
-// }else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "root",
-//     database: "burger2"
-//   });
-// }
+if(process.env.JAWS_DB){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burger2"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
